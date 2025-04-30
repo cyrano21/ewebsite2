b20eae10e615fb87bf3682a14a520689d49f6565
Louis Olivier <72631705+cyrano21@users.noreply.github.com>
2025-04-30 07:54:58 +0200

    Ajout du dossier components2 pour résoudre l'erreur d'importation du Badge

---
+++ components2/common/TextTruncate.tsx
@@ -0,0 +1,25 @@
+import React, { useMemo } from 'react';
+import Link from 'next/link';
+
+interface TextTruncateProps {
+  text: string;
+  maxLength: number;
+  url?: string;
+}
+
+const TextTruncate = ({ text, maxLength, url = '#!' }: TextTruncateProps) => {
+  const isTruncated = useMemo(() => {
+    return text.length > maxLength;
+  }, [text, maxLength]);
+
+  const displayText = isTruncated ? `${text.slice(0, maxLength)}...` : text;
+  return (
+    <>
+      {displayText}
+      {isTruncated && <Link to={url}>Read more</Link>}
+    </>
+  );
+};
+
+export default TextTruncate;
+
